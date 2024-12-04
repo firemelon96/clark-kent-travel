b798f7181333c0652fd30a15473b93407c7f2898
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2024-12-04 11:48:53 +0800

    fixed jomalia destination

--- app/data/logistics.ts
+++ app/data/logistics.ts
@@ -322,25 +322,25 @@ export const transfer_services = [
 
   {
     id: "jomalia-fastcraft-pps-to-coron",
-    service_name: "PPS to Coron Jomalia Fast Craft ",
+    service_name: "Elnido to Coron Jomalia Fast Craft ",
     vehicle_type: "cruise",
     type: "fast craft",
     capacity: 15,
     price_per_trip: 2800,
     description:
-      "Jomalia Shipping offers a convenient and scenic ferry service between Puerto Princesa City and Coron, Palawan. This route is perfect for travelers seeking a reliable and affordable way to explore the stunning islands of Palawan. Enjoy a comfortable journey aboard their well-maintained vessel, equipped with modern amenities to ensure a pleasant travel experience. Whether you're a tourist eager to discover Coron's iconic lagoons or a local traveling for business, Jomalia Shipping provides a seamless connection between these two beautiful destinations. Sit back, relax, and enjoy the breathtaking ocean views along the way.",
+      "Jomalia Shipping offers a convenient and scenic ferry service between Elnido and Coron, Palawan. This route is perfect for travelers seeking a reliable and affordable way to explore the stunning islands of Palawan. Enjoy a comfortable journey aboard their well-maintained vessel, equipped with modern amenities to ensure a pleasant travel experience. Whether you're a tourist eager to discover Coron's iconic lagoons or a local traveling for business, Jomalia Shipping provides a seamless connection between these two beautiful destinations. Sit back, relax, and enjoy the breathtaking ocean views along the way.",
     availability: "7:30 AM Daily",
     image: "/resources/logistics/jomalia.avif",
   },
   {
     id: "jomalia-fastcraft-coron-to-pps",
-    service_name: "Coron to PPS Jomalia Fast Craft ",
+    service_name: "Coron to Elnido Jomalia Fast Craft ",
     vehicle_type: "cruise",
     type: "fast craft",
     capacity: 15,
     price_per_trip: 2800,
     description:
-      "Jomalia Shipping offers a reliable and comfortable ferry service connecting Coron to Puerto Princesa City, Palawan. Ideal for travelers heading to Puerto Princesa's natural wonders, such as the Underground River or Honda Bay, this route ensures a hassle-free journey with excellent service. The ferry is equipped with modern facilities, providing a smooth and enjoyable ride as you traverse the scenic waters of Palawan. Whether you're a tourist or a local, Jomalia Shipping makes traveling between Coron and Puerto Princesa both convenient and unforgettable.",
+      "Jomalia Shipping offers a reliable and comfortable ferry service connecting Coron to Elnido, Palawan. Ideal for travelers heading to Puerto Princesa's natural wonders, such as the Underground River or Honda Bay, this route ensures a hassle-free journey with excellent service. The ferry is equipped with modern facilities, providing a smooth and enjoyable ride as you traverse the scenic waters of Palawan. Whether you're a tourist or a local, Jomalia Shipping makes traveling between Coron and Puerto Princesa both convenient and unforgettable.",
     availability: "12:30 PM Daily",
     image: "/resources/logistics/jomalia.avif",
   },
