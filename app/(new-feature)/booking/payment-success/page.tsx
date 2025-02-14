599c019fb24af19de804f2eebfab540dbb99ac06
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-02-14 20:52:16 +0800

    add fetch for api webhook

--- app/(new-feature)/booking/payment-success/page.tsx
+++ app/(new-feature)/booking/payment-success/page.tsx
@@ -1,7 +1,13 @@
-const SuccessPage = () => {
+const SuccessPage = async () => {
+  const data = await fetch(
+    "https://clark-kent-travel-git-update-firemelon96s-projects.vercel.app/api/webhook/receive_callbabck",
+  );
+
   return (
     <div>
       <h1>Successful Payment</h1>
+
+      {JSON.stringify(data)}
     </div>
   );
 };
