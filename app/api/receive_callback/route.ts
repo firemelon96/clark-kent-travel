f1035f0c62e7a70ee084d26e378de23158d37d70
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-02-14 23:35:51 +0800

    added the console log

--- app/api/receive_callback/route.ts
+++ app/api/receive_callback/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
+  console.log(body);
+
   if (body.status === "PAID") {
     console.log(
       `Invoice successfully paid with status ${body.status} and id ${body.id}`,
